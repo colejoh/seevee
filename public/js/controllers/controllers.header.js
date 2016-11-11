2c96faced9aa65d1104107db59663180a2055110
Cole Johnson <john1739@purdue.edu>
2016-11-11 01:02:13 -0500

    Added active tags

--- public/js/controllers/controllers.header.js
+++ public/js/controllers/controllers.header.js
@@ -1,8 +1,32 @@
 seevee.controller("headerCtrl", ['$scope', '$location', '$window',
   function($scope, $location, $window) {
-    
+    $scope.path = $location.$$path;
+    assignClasses();
+
+    // $scope.accomplishmentClass = 'nav-unselected';
+    // $scope.resumeClass = 'nav-unselected';
+    // $scope.accountClass = 'nav-unselected';
+
     $scope.go = function(location) {
+      $scope.path = location;
+      assignClasses();
       $location.path(location);
     };
+
+    function assignClasses() {
+      if($scope.path == '/') {
+        $scope.accomplishmentClass = 'nav-selected';
+        $scope.resumeClass = 'nav-unselected';
+        $scope.accountClass = 'nav-unselected';
+      } else if ($scope.path == '/resumes') {
+        $scope.accomplishmentClass = 'nav-unselected';
+        $scope.resumeClass = 'nav-selected';
+        $scope.accountClass = 'nav-unselected';
+      } else if ($scope.path == '/account') {
+        $scope.accomplishmentClass = 'nav-unselected';
+        $scope.resumeClass = 'nav-unselected';
+        $scope.accountClass = 'nav-selected';
+      }
+    }
   }
 ]);
