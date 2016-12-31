ee8cd2b8cdd9cf7553d5719fc6f18504e14321c7
Cole Johnson <john1739@purdue.edu>
2016-12-31 18:46:41 -0500

    Created full API for pages

--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,7 +1,53 @@
 var router = require('express').Router();
+var User = require('../models/user');
+var Accomplishment = require('../models/accomplishment');
 
 router.get('/:pageName', function(req, res) {
-    res.json({ name : req.params.pageName});
+    User.findOne({pageName: req.params.pageName}, function(err, user) {
+        if (err) res.send(err);
+        if(user) {
+            Accomplishment.find({userId: user._id}, function(err, accomplishments) {
+                var work = [];
+                for(var i = 0; i < accomplishments.length; i++) {
+                    if(accomplishments[i].type === 'work') work.unshift(accomplishments[i]);
+                }
+                work = sortByKey(work, "dateEnd");
+
+                var data = {};
+                data.firstName = user.firstName;
+                data.lastName = user.lastName;
+                data.workTitle = work[0].title;
+                data.workOrigin = work[0].origin;
+
+                res.json(data);
+            });
+        } else {
+            User.findOne({_id: req.params.pageName}, function(err, user) {
+                Accomplishment.find({userId: user._id}, function(err, accomplishments) {
+                    var work = [];
+                    for(var i = 0; i < accomplishments.length; i++) {
+                        if(accomplishments[i].type === 'work') work.unshift(accomplishments[i]);
+                    }
+                    work = sortByKey(work, "dateEnd");
+
+                    var data = {};
+                    data.firstName = user.firstName;
+                    data.lastName = user.lastName;
+                    data.workTitle = work[0].title;
+                    data.workOrigin = work[0].origin;
+
+                    res.json(data);
+                });
+            });
+        }
+    });
 });
+
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
 
 module.exports = router;
