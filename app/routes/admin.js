421542c73b52c9d336d3dffca36529378b8487de
Cole Johnson <john1739@purdue.edu>
2017-01-02 22:26:43 -0500

    Added authentication check

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -2,21 +2,27 @@ var router = require('express').Router();
 var User = require('../models/user');
 
 router.get('/', function(req, res) {
-    if(req.session.passport.user.email === 'cjohnson199@gmail.com') {
-        User.find(function(err, users) {
-            if(err) res.send(err);
-            res.json(users);
-        });
-    } else {
-        res.redirect('/#/');
+    if(typeof req.session.passport == 'undefined') res.json({error: 'Not Authenticated'});
+    else {
+        if(req.session.passport.user.email === 'cjohnson199@gmail.com') {
+            User.find(function(err, users) {
+                if(err) res.send(err);
+                res.json(users);
+            });
+        } else {
+            res.redirect('/#/');
+        }
     }
 });
 
 router.get('/isAdmin', function(req, res) {
-    if(req.session.passport.user.email === 'cjohnson199@gmail.com') {
-        res.json({'isAdmin': true});
-    } else {
-        res.json({'isAdmin': false});
+    if(typeof req.session.passport == 'undefined') res.json({error: 'Not Authenticated'});
+    else {
+        if(req.session.passport.user.email === 'cjohnson199@gmail.com') {
+            res.json({'isAdmin': true});
+        } else {
+            res.json({'isAdmin': false});
+        }
     }
 });
 
--- app/routes/skill.js
+++ app/routes/skill.js
@@ -6,11 +6,14 @@ var Skill = require('../models/skill');
  *      that belong to the current user
  */
 router.get('/', function(req, res) {
-    var sessionId = req.session.passport.user._id;
-    Skill.find({userId: sessionId}, function(err, skills) {
-        if(err) res.send(err);
-        res.send(skills);
-    });
+    if(typeof req.session.passport == 'undefined') res.json({error: 'Not Authenticated'});
+    else {
+        var sessionId = req.session.passport.user._id;
+        Skill.find({userId: sessionId}, function(err, skills) {
+            if(err) res.send(err);
+            res.send(skills);
+        });
+    }
 });
 
 module.exports = router;
